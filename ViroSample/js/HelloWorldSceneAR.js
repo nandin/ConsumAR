a27c74a573206a6d9f4e2deaac508d0ed3402d26
Sidharth Rampally <sidharth.rampally@reancloud.com>
2018-02-17 17:50:34 -0500

    bod push

--- ViroSample/js/HelloWorldSceneAR.js
+++ ViroSample/js/HelloWorldSceneAR.js
@@ -29,16 +29,16 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene onTrackingInitialized={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.3, .3, .3]} position={[0, 0, -2.4]} style={{fontFamily:"Helvetica", fontSize:30, color:"#000000"}} opacity={1.0}/>
-        <ViroSurface height={0.4} width={0.4} materials={["earth"]} position={[0,0,-2.5]} opacity={0.6}/>
-        
+        <ViroText text={this.state.text} scale={[.2, .2, .2]} position={[0, 0, -2.4]} style={{fontFamily:"Helvetica", fontSize:30, color:"#000000"}} opacity={1.0}/>
+        <ViroSurface height={0.5} width={0.5} materials={["earth"]} position={[0,0,-2.5]} opacity={0.6}/>
+
       </ViroARScene>
     );
   }
 
   _onInitialized() {
     this.setState({
-      text : "Hello World!"
+      text : ""
     });
   }
 
